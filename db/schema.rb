be3aff2a340c1b43702ffda26b12b753b2de3b32
myeongseok.jeong <myeongseok.jeong@litalico.co.jp>
2015-05-24 20:27:39 +0900

    before list 10.21

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522065607) do
+ActiveRecord::Schema.define(version: 20150524111818) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
